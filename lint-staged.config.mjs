ad4aa4f2e9517bdb5eb7e9c915b7ce0afe9c6686
Harsh <onlybelieveharsh@gmail.com>
2026-04-27 21:39:12 +0530

    chore: setup project configuration and base assets

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
-  "*.{js,jsx,ts,tsx}": ["prettier --write", "eslint --fix"],
-  "*.{mjs,json,css,md}": ["prettier --write"],
+  '*.{js,jsx,ts,tsx}': ['prettier --write', 'eslint --fix'],
+  '*.{mjs,json,css,md}': ['prettier --write'],
 };
